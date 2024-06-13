cc1c5dd9dbd927994f056abc4cb043c76b28bb0d
Serena Gibbons <serenalgibbons@gmail.com>
2024-06-12 22:11:29 -0400

    Update comments

--- bt/decorators/until_fails.py
+++ bt/decorators/until_fails.py
@@ -1,22 +1,11 @@
-
 import bt_library as btl
 
 class UntilFails(btl.Decorator):
     
-    def __init__(self, child: btl.TreeNode):
-        """
-        Default constructor.
-
-        :param time: Duration of the until fails
-        :param child: Child associated to the decorator
-        """
-        super().__init__(child)
-    
     """
     Implementation of the Decorator "Until Fails".
-    Returns RUNNING when the child returns
-    SUCCEEDED or RUNNING. It returns SUCCEEDED when the
-    child returns FAILED
+    Returns RUNNING when the child returns SUCCEEDED or RUNNING. 
+    It returns SUCCEEDED when the child returns FAILED.
     """
     def run(self, blackboard: btl.Blackboard) -> btl.ResultEnum:
         self.print_message("Running until failure")
@@ -24,8 +13,9 @@ class UntilFails(btl.Decorator):
         # Evaluate the child
         result_child = self.child.run(blackboard)
 
-        # If the child failed, terminate immediately the timer
+        # If the child returned succeeded or running, return running
         if result_child == btl.ResultEnum.SUCCEEDED or result_child == btl.ResultEnum.RUNNING:
             return self.report_running(blackboard)
 
+        # else the child failed, return succeeded
         return self.report_succeeded(blackboard)
